d3b0c2b5932d80e96e14638f41a5f10c8b9ba925
MeltingOpera <85198240+MeltingOpera@users.noreply.github.com>
2023-05-21 22:11:59 +0200

    Update page.tsx

--- modal-test/app/page.tsx
+++ modal-test/app/page.tsx
@@ -4,6 +4,7 @@ export default function Home() {
   return (
     <main>
       <h1 className="text-3xl font-bold mb-5">Movie List</h1>
+      {/* @ts-expect-error Server Component */}
       <Movielist />
     </main>
   );
